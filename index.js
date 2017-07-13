ef3277ac8f58c23975de056f77e3023f1d7a466a
Kevin Curry <kmcurry@gmail.com>
2017-07-13 14:44:18 -0400

    search for comparity

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.get('/test', function (req, res) {
         .forBrowser('phantomjs')
         .build();
     driver.get('http://www.google.com/ncr');
-    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.name('q')).sendKeys('comparity');
     driver.findElement(By.name('btnG')).click();
     driver.wait(function() {
         return driver.getTitle().then(function(title) {
             console.log(title);
-            return title === 'webdriver - Google Search';
+            return title === 'comparity - Google Search';
         });
     }, 5000).then(function() {
         res.status(200).send('Done');
